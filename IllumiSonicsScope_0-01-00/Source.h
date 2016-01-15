f9013877bb4ab9ff1b097ac3c6f9398acff4bc73
klbell <klbell@ualberta.ca>
2016-01-15 13:45:37 -0700

    FirstCommit

---
+++ IllumiSonicsScope_0-01-00/Source.h
@@ -0,0 +1,31 @@
+
+
+#ifndef SOURCE_H
+#define SOURCE_H
+
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <time.h>
+//#include <iostream>
+#include "nidaqmxNew.h"
+#include "StageMoves.h"
+#include "GageFuncs.h"
+#include "DAQmxErrors.h"
+#include <math.h>
+#include "CsAppSupport.h"
+#include "CsTchar.h"
+#include "CsSdkMisc.h"
+#include "MiscFuncs.h"
+#include "HilbertMath.h"
+#include "MediaFuncs.h"
+
+
+int runPARS();
+int runPARSRT();
+int runPAM();
+int runFree();
+
+
+#endif
